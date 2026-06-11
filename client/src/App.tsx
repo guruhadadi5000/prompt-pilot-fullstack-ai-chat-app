be27fd2f171b02d2018ae449f767c7090dab6141
Guru Hadadi <guru.hadadi5000@gmail.com>
2026-06-12 00:41:45 +1000

    render sidebar with dummy data and create the context plus more component files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,35 @@
+import { Route, Routes } from "react-router-dom";
+import SideBar from "./components/SideBar";
+import ChatBox from "./components/ChatBox";
+import Credits from "./pages/Credits";
+import Community from "./pages/Community";
+import { useState } from "react";
+import { assets } from "./assets/assets";
+
 const App = () => {
-  return <div>App</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <>
+      {!isMenuOpen && (
+        <img
+          onClick={() => setIsMenuOpen(true)}
+          src={assets.menu_icon}
+          alt="menu"
+          className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
+        />
+      )}
+      <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <SideBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ client/src/context/AppContext.tsx
@@ -0,0 +1,60 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { dummyChats, dummyUserData } from "../assets/assets";
+
+const AppContext = createContext(null);
+
+export const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [chats, setChats] = useState([]);
+  const [selectedChat, setSelectedChat] = useState(null);
+  const [theme, setTheme] = useState(localStorage.getItem("theme") || "light");
+
+  const fetchUser = async () => {
+    setUser(dummyUserData);
+  };
+
+  const fetchUsersChat = async () => {
+    setChats(dummyChats);
+    setSelectedChat(dummyChats[0]);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  useEffect(() => {
+    if (user) {
+      fetchUsersChat();
+    } else {
+      setChats([]);
+      setSelectedChat(null);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", theme);
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    fetchUser,
+    chats,
+    setChats,
+    selectedChat,
+    setSelectedChat,
+    theme,
+    setTheme,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => useContext(AppContext);
