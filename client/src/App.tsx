e94f9a1cbaa8b8f78faf60cee6a17c2b4d2d6f34
Guru Hadadi <guru.hadadi5000@gmail.com>
2026-06-12 14:32:17 +1000

    credits, community and login page working with dummy data

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,10 +6,13 @@ import Community from "./pages/Community";
 import { useState } from "react";
 import { assets } from "./assets/assets";
 import Loading from "./pages/Loading";
+import Login from "./pages/Login";
+import { useAppContext } from "./context/AppContext";
 
 const App = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { pathname } = useLocation();
+  const { user } = useAppContext();
 
   if (pathname === "/loading") {
     return <Loading />;
@@ -25,16 +28,22 @@ const App = () => {
           className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
         />
       )}
-      <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
-        <div className="flex h-screen w-screen">
-          <SideBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
-          <Routes>
-            <Route path="/" element={<ChatBox />} />
-            <Route path="/credits" element={<Credits />} />
-            <Route path="/community" element={<Community />} />
-          </Routes>
+      {user ? (
+        <div className="dark:bg-linear-to-b from-[#242124] to-[#000000] dark:text-white">
+          <div className="flex h-screen w-screen">
+            <SideBar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+            <Routes>
+              <Route path="/" element={<ChatBox />} />
+              <Route path="/credits" element={<Credits />} />
+              <Route path="/community" element={<Community />} />
+            </Routes>
+          </div>
         </div>
-      </div>
+      ) : (
+        <div className="bg-linear-to-b from-[#242124] to-[#000000] flex items-center justify-center h-screen w-screen">
+          <Login />
+        </div>
+      )}
     </>
   );
 };
