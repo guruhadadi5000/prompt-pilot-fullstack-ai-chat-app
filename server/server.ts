835e61acd3b23f277f46d3e777a5c47a9930c7e3
Guru Hadadi <guru.hadadi5000@gmail.com>
2026-06-12 21:16:10 +1000

    set up user modal, route and user registeration api

--- server/server.ts
+++ server/server.ts
@@ -1,19 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import connectDB from "./configs/db";
+import userRouter from "./routes/userRouter";
 const app = express();
 
 app.use(express.json());
+dotenv.config();
+await connectDB();
 app.use(cors());
 
-dotenv.config();
 const PORT = process.env.PORT || 3000;
 
 // Routes
 app.get("/", (_, res) => {
   res.send("Server is running");
 });
+
+app.use("/api/users", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
